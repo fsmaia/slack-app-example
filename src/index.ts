7f37e84b33ab4c334bcaad185141f99ba16860da
Fernando Maia <fernando.maia@ifood.com.br>
2020-06-12 01:40:09 -0300

    Add environment variable options parser

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,17 @@ dotenv.config();
 
 const port: string | number = process.env.PORT || 5000;
 
+const botAccessToken = process.env.BOT_ACCESS_TOKEN;
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const signingSecret = process.env.SIGNING_SECRET;
+
+if (!botAccessToken || !clientId || !clientSecret || !signingSecret) {
+  throw new Error(
+    'Proper environment variables must be provided! Refer to the project README.'
+  );
+}
+
 const app = express();
 
 app.use(express.json());
